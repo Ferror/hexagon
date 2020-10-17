1f5674d78cd48100f03c72b78635aaba8608209f
ferror <zmalcherczyk@gmail.com>
2020-10-17 18:21:33 +0200

    GraphicApp

---
+++ src/presenter/graphic/GraphicApp.java
@@ -0,0 +1,11 @@
+package presenter.graphic;
+
+import javax.swing.*;
+
+public class GraphicApp
+{
+    public static void main(String[] args)
+    {
+        JOptionPane.showMessageDialog(null, "Messaage", "Welcome Message", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
