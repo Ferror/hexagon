77120719b4325700f8400d569c5bdb8e1100e1d7
ferror <zmalcherczyk@gmail.com>
2020-10-17 18:30:23 +0200

    change assertions

--- test/domain/NumberTest.java
+++ test/domain/NumberTest.java
@@ -1,6 +1,6 @@
 package domain;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class NumberTest
@@ -9,13 +9,13 @@ public class NumberTest
     public void testItAdds()
     {
         Number number = new Number(2);
-        assertEquals(2, number.toInteger());
+        Assertions.assertEquals(2, number.toInteger());
     }
 
     @Test
     public void testItFails()
     {
         Number number = new Number(2);
-        assertEquals(1, number.toInteger());
+        Assertions.assertEquals(1, number.toInteger());
     }
 }
