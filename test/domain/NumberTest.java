5e9b703c4c4b697b0bad98226709d5c27d7176bb
ferror <zmalcherczyk@gmail.com>
2020-11-12 00:04:43 +0100

    CurrencyTest

--- test/domain/NumberTest.java
+++ test/domain/NumberTest.java
@@ -6,9 +6,16 @@ import org.junit.jupiter.api.Test;
 public class NumberTest
 {
     @Test
-    public void testItAdds()
+    public void testItTranslatesToInteger()
     {
         Number number = new Number(2);
         Assertions.assertEquals(2, number.toInteger());
+    }
+
+    @Test
+    public void testItAdds()
+    {
+        Number number = new Number(2);
+        Assertions.assertEquals(6, number.add(new Number(4)).toInteger());
     }
 }
