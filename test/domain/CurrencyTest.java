fa129c09aec5f012ece39e7976c95fcaa674a793
ferror <zmalcherczyk@gmail.com>
2020-11-12 00:04:48 +0100

    CurrencyTest

---
+++ test/domain/CurrencyTest.java
@@ -0,0 +1,26 @@
+package domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.util.HashMap;
+
+public class CurrencyTest
+{
+    @Test
+    public void itGetsName()
+    {
+        Currency currency = new Currency("Dollar", "USD");
+        Assertions.assertEquals("Dollar", currency.getName());
+        Assertions.assertEquals("USD", currency.getIso());
+    }
+
+    @Test
+    public void itSerializes()
+    {
+        Currency currency = new Currency("Dollar", "USD");
+        HashMap<String, String> items = new HashMap<>();
+        items.put("name", "Dollar");
+        items.put("iso", "USD");
+        Assertions.assertEquals(items, currency.serialize());
+    }
+}
