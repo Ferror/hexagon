12e29238dafb8e3d59861e8941f67ffd4b009d00
ferror <zmalcherczyk@gmail.com>
2020-11-16 19:37:13 +0100

    Remove not used classes

--- src/infrastructure/memory/MemoryEventStore.java
+++ src/infrastructure/memory/MemoryEventStore.java
@@ -8,7 +8,12 @@ import java.util.List;
 
 public class MemoryEventStore implements EventStore
 {
-    private final List<Event> events = new ArrayList<>();
+    private final List<Event> events;
+
+    public MemoryEventStore()
+    {
+        this.events = new ArrayList<>();
+    }
 
     @Override
     public void store(Event event)
