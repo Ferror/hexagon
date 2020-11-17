fd77e42c05f55561b539b9dfb857309de2d6d86c
ferror <zmalcherczyk@gmail.com>
2020-11-17 21:39:26 +0100

    test routing

--- test/presenter/rest/http/RoutingTest.java
+++ test/presenter/rest/http/RoutingTest.java
@@ -16,9 +16,12 @@ public class RoutingTest
 
         routing.addRouting("/test", "GET", new FakeAction());
         routing.addRouting("/another", "GET", new RouteNotFoundAction());
+        routing.addRouting("/another", "POST", new FakeAction());
 
         try {
             Assertions.assertTrue(routing.findAction("/test", "GET") instanceof FakeAction);
+            Assertions.assertTrue(routing.findAction("/another", "POST") instanceof FakeAction);
+            Assertions.assertTrue(routing.findAction("/another", "GET") instanceof RouteNotFoundAction);
         } catch (Exception exception) {
             Assertions.fail("exception");
         }
