fd04f6770b22b3d3da786e0f62155ca831fa7d3b
ferror <zmalcherczyk@gmail.com>
2020-11-17 21:13:16 +0100

    hashcode test

--- test/domain/CurrencyTest.java
+++ test/domain/CurrencyTest.java
@@ -38,5 +38,13 @@ public class CurrencyTest
         Assertions.assertTrue(currency.equals(currency));
         Assertions.assertFalse(currency.equals(null));
         Assertions.assertFalse(currency.equals(new Object()));
+        Assertions.assertEquals(-793283637, currency.hashCode());
+    }
+
+    @Test
+    public void testItGetsHashCode()
+    {
+        Currency currency = new Currency("Dollar", "USD");
+        Assertions.assertEquals(-793283637, currency.hashCode());
     }
 }
